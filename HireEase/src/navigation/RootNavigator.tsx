52d51e3045f401b885fa19aefe6d71ad8c48de15
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-02-20 22:50:31 +0500

    Added basic Auth Navigator to the root navigator

--- HireEase/src/navigation/RootNavigator.tsx
+++ HireEase/src/navigation/RootNavigator.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+
+import AuthNavigator from "./AuthNavigator";
 import TabNavigator from "./TabNavigator";
 
 const Stack = createStackNavigator();
@@ -7,6 +9,7 @@ const Stack = createStackNavigator();
 export default function RootNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Auth" component={AuthNavigator} />
       <Stack.Screen name="TabNavigator" component={TabNavigator} />
     </Stack.Navigator>
   );
