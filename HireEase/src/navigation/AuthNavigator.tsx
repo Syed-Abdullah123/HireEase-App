758784129ba84681f9f9c43be5ad212ad856ea68
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-02-20 22:52:31 +0500

    Auth navigator containing the auth screens

---
+++ HireEase/src/navigation/AuthNavigator.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Platform } from "react-native";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
+
+import WelcomeScreen from "../screens/WelcomeScreen";
+import SignupScreen from "../screens/SignupScreen";
+import SigninScreen from "../screens/SigninScreen";
+
+const Stack = createStackNavigator();
+
+export default function AuthNavigator() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Welcome"
+      screenOptions={{ headerShown: false, presentation: "modal" }}
+    >
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{
+          ...TransitionPresets.FadeFromBottomAndroid, // Adds a fade + slight slide-up effect
+        }}
+      />
+      <Stack.Screen
+        name="Register"
+        component={SignupScreen}
+        options={{
+          gestureEnabled: true,
+          cardStyle: {
+            backgroundColor: "transparent",
+            ...TransitionPresets.ModalSlideFromBottomIOS,
+          },
+        }}
+      />
+      <Stack.Screen
+        name="Signin"
+        component={SigninScreen}
+        options={{
+          gestureEnabled: true,
+          cardStyle: {
+            backgroundColor: "transparent",
+            ...TransitionPresets.ModalSlideFromBottomIOS,
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
