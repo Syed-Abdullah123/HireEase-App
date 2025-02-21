2025054fbe49c25aa1ac205e0fbba111d3a447ff
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-02-21 22:58:24 +0500

    feat: PaperProvider for the react-native-paper library

--- HireEase/App.tsx
+++ HireEase/App.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import RootNavigator from './src/navigation/RootNavigator';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
+
+import { NavigationContainer } from "@react-navigation/native";
+import RootNavigator from "./src/navigation/RootNavigator";
+
+import { Provider as PaperProvider } from "react-native-paper";
+
+const theme = {
+  fonts: {
+    regular: { fontFamily: "Lexend-Regular" },
+    medium: { fontFamily: "Lexend-Medium" },
+    light: { fontFamily: "Lexend-Light" },
+    bold: { fontFamily: "Lexend-Bold" },
+    bodySmall: { fontFamily: "Lexend-Regular" },
+    bodyMedium: { fontFamily: "Lexend-Regular" },
+    bodyLarge: { fontFamily: "Lexend-Regular" },
+  },
+};
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Lexend-Light": require("./assets/fonts/Lexend-Light.ttf"),
+    "Lexend-Regular": require("./assets/fonts/Lexend-Regular.ttf"),
+    "Lexend-Medium": require("./assets/fonts/Lexend-Medium.ttf"),
+    "Lexend-Bold": require("./assets/fonts/Lexend-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <NavigationContainer>
-      <RootNavigator />
-      <StatusBar style="auto" />
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <RootNavigator />
+        <StatusBar style="auto" />
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
