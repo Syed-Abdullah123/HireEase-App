bd79dc8a469b72e3278105d31efbabd28c736394
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-02-21 22:56:46 +0500

    modified the signin screen with better stylings

--- HireEase/src/screens/SigninScreen.tsx
+++ HireEase/src/screens/SigninScreen.tsx
@@ -1,16 +1,70 @@
-import React from "react";
-import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { TextInput } from "react-native-paper";
+
+import PrimaryButton from "../components/PrimaryButton";
 
 export default function SigninScreen({ navigation }: any) {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>SigninScreen</Text>
       <TouchableOpacity
         onPress={() => navigation.goBack()}
-        style={styles.closeButton}
+        style={styles.backIcon}
       >
-        <Text style={{ fontSize: 18 }}>✕</Text>
+        <Ionicons name="arrow-back" size={24} color="black" />
       </TouchableOpacity>
+      <Text style={styles.title}>
+        Back Again? Let’s Make Your Resume{" "}
+        <Text style={{ color: "#5B2333", textDecorationLine: "underline" }}>
+          Even Better!
+        </Text>
+      </Text>
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          label="Email"
+          mode="outlined"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          style={styles.input}
+          activeOutlineColor="#5B2333"
+          theme={{ roundness: 10 }}
+        />
+        <TextInput
+          label="Password"
+          mode="outlined"
+          secureTextEntry={!passwordVisible}
+          right={
+            <TextInput.Icon
+              icon={!passwordVisible ? "eye-off" : "eye"}
+              onPress={() => setPasswordVisible(!passwordVisible)}
+              color={"#555"}
+              size={20}
+              hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
+            />
+          }
+          style={styles.input}
+          activeOutlineColor="#5B2333"
+          theme={{ roundness: 10 }}
+        />
+      </View>
+
+      <Text style={styles.forgotText}>Forgot Password?</Text>
+
+      <PrimaryButton
+        title="Sign In"
+        onPress={() => navigation.navigate("Home")}
+      />
+
+      <View style={styles.bottomText}>
+        <Text style={styles.text}>Don't have an account?</Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -20,20 +74,51 @@ const styles = StyleSheet.create({
     position: "absolute",
     bottom: 0,
     width: "100%",
-    height: "90%", // Controls the modal height effect
-    backgroundColor: "white",
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
+    height: "92%",
+    backgroundColor: "#F7F4F3",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
     padding: 20,
   },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "center",
+  backIcon: {
+    width: 30,
+    height: 30,
   },
-  closeButton: {
-    position: "absolute",
-    top: 20,
-    right: 20,
+  title: {
+    fontFamily: "Lexend-Medium",
+    fontSize: 20,
+    textAlign: "center",
+    marginTop: 20,
+  },
+  inputContainer: {
+    marginTop: 20,
+    gap: 15,
+  },
+  input: {
+    fontSize: 14,
+    backgroundColor: "transparent",
+  },
+  forgotText: {
+    fontFamily: "Lexend-Medium",
+    fontSize: 14,
+    textAlign: "right",
+    marginTop: 10,
+    color: "#5B2333",
+  },
+  bottomText: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+    gap: 3,
+  },
+  text: {
+    fontFamily: "Lexend-Regular",
+    fontSize: 14,
+  },
+  buttonText: {
+    fontFamily: "Lexend-Medium",
+    fontSize: 14,
+    color: "#5B2333",
   },
 });
