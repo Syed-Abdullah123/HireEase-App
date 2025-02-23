db07ff872fad9278ceae1c0a19592bc2ca86e1ae
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-02-23 11:55:27 +0500

    Modified the background color according to the theme

--- HireEase/src/screens/HomeScreen.tsx
+++ HireEase/src/screens/HomeScreen.tsx
@@ -1,19 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import HomeOutlineIcon from "../../assets/svg/HomeOutline";
+import HomeFilledIcon from "../../assets/svg/HomeFilled";
+import DocumentOutlineIcon from "../../assets/svg/DocumentOutline";
+import DocumentFilledIcon from "../../assets/svg/DocumentFilled";
+import SettingOutlineIcon from "../../assets/svg/SettingOutline";
+import SettingFilledIcon from "../../assets/svg/SettingFilled";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>HomeScreen</Text>
+      <HomeOutlineIcon />
+      <HomeFilledIcon />
+      <DocumentOutlineIcon />
+      <DocumentFilledIcon />
+      <SettingOutlineIcon />
+      <SettingFilledIcon />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-})
+    backgroundColor: "#F7F4F3",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 15,
+  },
+});
