27b2aee2245f8fcfda093ca027674b90c25e9931
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-02-24 22:21:00 +0500

    called the ATSCheckCard component on homscreen

--- HireEase/src/screens/HomeScreen.tsx
+++ HireEase/src/screens/HomeScreen.tsx
@@ -1,21 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import HomeOutlineIcon from "../../assets/svg/HomeOutline";
-import HomeFilledIcon from "../../assets/svg/HomeFilled";
-import DocumentOutlineIcon from "../../assets/svg/DocumentOutline";
-import DocumentFilledIcon from "../../assets/svg/DocumentFilled";
-import SettingOutlineIcon from "../../assets/svg/SettingOutline";
-import SettingFilledIcon from "../../assets/svg/SettingFilled";
+import ATSCheckCard from "../components/ATSCheckCard";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <HomeOutlineIcon />
-      <HomeFilledIcon />
-      <DocumentOutlineIcon />
-      <DocumentFilledIcon />
-      <SettingOutlineIcon />
-      <SettingFilledIcon />
+      <ATSCheckCard />
     </View>
   );
 }
@@ -24,8 +14,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#F7F4F3",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 15,
+    padding: 15,
   },
 });
