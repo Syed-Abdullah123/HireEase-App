5d675f3f457c4fd3ced194fabd138c55d65d109b
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-02-25 21:30:26 +0500

    Modified the card and using the MyResumeComponent

--- HireEase/src/screens/HomeScreen.tsx
+++ HireEase/src/screens/HomeScreen.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import ATSCheckCard from "../components/ATSCheckCard";
+import MyResumeComponent from "../components/MyResumeComponent";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <ATSCheckCard />
+      <Text style={styles.title}>My Resumes</Text>
+      <MyResumeComponent />
     </View>
   );
 }
@@ -15,5 +18,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#F7F4F3",
     padding: 15,
+  },
+  title: {
+    fontFamily: "Lexend-Medium",
+    fontSize: 20,
+    marginTop: 20,
+    marginBottom: 20,
+    color: "#333",
   },
 });
