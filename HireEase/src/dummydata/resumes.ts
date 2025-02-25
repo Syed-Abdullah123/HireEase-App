e474fcdbd162a2f874410527a2e7b9aaaf5e7406
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-02-25 21:29:01 +0500

    DummyData to display fake resumes

---
+++ HireEase/src/dummydata/resumes.ts
@@ -0,0 +1,25 @@
+const resumes = [
+    {
+        id: 1,
+        image: require("../../assets/images/CV-pic-1.jpg"),
+        title: "UI / UX Designer",
+        style: "Modern",
+        lastupdate: "3 days ago", 
+    },
+    {
+        id: 2,
+        image: require("../../assets/images/CV-pic-2.jpg"),
+        title: "Mobile App Developer",
+        style: "Professional",
+        lastupdate: "2 days ago", 
+    },
+    {
+        id: 3,
+        image: require("../../assets/images/CV-pic-3.jpg"),
+        title: "Product Designer",
+        style: "Minimalist",
+        lastupdate: "4 days ago", 
+    },
+]
+
+export default resumes
