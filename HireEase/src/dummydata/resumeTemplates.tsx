f6ae34762c5070c39a8c564c7900965a5d9bb6c1
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-03-02 22:53:48 +0500

    dummy resume templates to display

---
+++ HireEase/src/dummydata/resumeTemplates.tsx
@@ -0,0 +1,38 @@
+export const templates = [
+  {
+    id: 1,
+    title: "Celeste",
+    style: "Minimalist",
+    image: require("../../assets/images/CV-pic-1.jpg"),
+  },
+  {
+    id: 2,
+    title: "Aurora",
+    style: "Professional",
+    image: require("../../assets/images/CV-pic-2.jpg"),
+  },
+  {
+    id: 3,
+    title: "Bianca",
+    style: "Modern",
+    image: require("../../assets/images/CV-pic-3.jpg"),
+  },
+  {
+    id: 4,
+    title: "Estelle",
+    style: "Simple",
+    image: require("../../assets/images/CV-pic-1.jpg"),
+  },
+  {
+    id: 5,
+    title: "Celeste",
+    style: "Minimalist",
+    image: require("../../assets/images/CV-pic-2.jpg"),
+  },
+  {
+    id: 6,
+    title: "Aurora",
+    style: "Professional",
+    image: require("../../assets/images/CV-pic-3.jpg"),
+  },
+];
