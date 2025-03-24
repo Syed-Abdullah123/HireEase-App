a7939d72facdbeb8340605c610e0103d409e7afd
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-03-24 12:32:37 +0500

    added the resume template as a screen

--- HireEase/src/navigation/RootNavigator.tsx
+++ HireEase/src/navigation/RootNavigator.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import AuthNavigator from "./AuthNavigator";
 import TabNavigator from "./TabNavigator";
+import ResumeTemplate from "../templates/resume1/resume";
 
 const Stack = createStackNavigator();
 
@@ -15,6 +16,7 @@ export default function RootNavigator() {
     >
       <Stack.Screen name="Auth" component={AuthNavigator} />
       <Stack.Screen name="TabNavigator" component={TabNavigator} />
+      <Stack.Screen name="Resume" component={ResumeTemplate} />
     </Stack.Navigator>
   );
 }
